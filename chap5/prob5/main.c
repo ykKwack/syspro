8400a20d18a6198d3c75a631405e53066cd03b98
ykKwack <sofiajjang@gmail.com>
2024-10-31 02:10:24 +0000

    chap7/prob1

--- chap5/prob5/main.c
+++ chap5/prob5/main.c
@@ -4,23 +4,23 @@
 #include <fcntl.h>
 #include "student.h"
 
-int main(int argc, char argv[])
+main(int argc, char *argv[])
 {
 	int fd;
 	struct student record;
 	if(argc<2) 
 	{
-		fprintf(stderr "How to use: %s file \n", argv[0]);
+		fprintf(stderr, "How to use: %s file \n", argv[0]);
 		exit(1);
 	}
 
-	if((fd = open(arv[1], O_WRONLY | O_CREAT | O_EXCL, 0640))==-1)
+	if((fd = open(argv[1], O_WRONLY | O_CREAT | O_EXCL, 0640))==-1)
 	{
 		perror(argv[1]);
 		exit(2);
 	}
 	printf("%-9s %-8s %-4s\n", "StuID", "Name", "Score");
-	while (scanf("%d %s %d", &record.id, record.name &reord.score) ==3)
+	while (scanf("%d %s %d", &record.id, record.name, &record.score) ==3)
 	{
 		lseek(fd, (record.id - START_ID) * sizeof(record), SEEK_SET);
 		write(fd, (char *) &record, sizeof(record));
