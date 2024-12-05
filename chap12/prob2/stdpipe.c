7af57260c27485a6294ef8c395726933353eac1d
ykKwack <sofiajjang@gmail.com>
2024-12-05 02:01:22 +0000

    chapter12-prob6

---
+++ chap12/prob2/stdpipe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define MAXLINE 100
+
+int main(int argc, char** argv)
+{
+	int n, pid, fd[2];
+	char line [MAXLINE];
+
+	pipe(fd);
+
+	if((pid = fork()) == 0)
+	{
+		close(fd[0]);
+		dup2(fd[1],1);
+		close(fd[1]);
+		printf("Hello! pipe\n");
+		printf("Bye! pipe\n");
+	}
+	else
+	{
+		close(fd[1]);
+		printf("result from child process\n");
+		while((n=read(fd[0], line, MAXLINE))>0)
+			write(STDOUT_FILENO, line, n);
+	}
+	exit(0);
+}
---
+++ chap12/prob6/npreader.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define MAXLINE 100
+
+int main()
+{
+	int fd;
+	char str[MAXLINE];
+	unlink("myPipe");
+	mkfifo("myPipe", 0666);
+	fd = open("myPipe", O_RDONLY);
+	while(readLine(fd, str))
+		printf("%s \n", str);
+	close(fd);
+	return 0;
+}
+
+int readLine(int fd, char *str)
+{
+	int n;
+	do
+	{
+		n=read(fd, str, 1); 
+	} while (n>0 && *str++ != NULL);
+	return (n>0);
+}
